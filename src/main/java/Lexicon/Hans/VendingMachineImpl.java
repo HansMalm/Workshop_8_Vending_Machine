d6d29330ef8565f6f61b387c46462933807e6f2e
Hans Malmefjäll <hans.malmefjall@gmail.com>
2026-03-26 14:36:07 +0100

    mod VendingMachineImpl.java class file
    add override methods from IVendingMachine class.

--- src/main/java/Lexicon/Hans/VendingMachineImpl.java
+++ src/main/java/Lexicon/Hans/VendingMachineImpl.java
@@ -1,5 +1,61 @@
 package Lexicon.Hans;
 
+import java.util.ArrayList;
+
 public class VendingMachineImpl implements IVendingMachine {
-    IVendingMachine vendingMachine;
+
+   private Product product;
+   private int balance;
+   private ArrayList<Product> productList = new ArrayList<>();
+
+    @Override
+    public void insertCoin(int coin) {
+        switch (coin) {
+            case 1:
+                balance += 1;
+                break;
+            case 2:
+                balance += 2;
+                break;
+            case 5:
+                balance += 5;
+                break;
+            case 10:
+                balance += 10;
+                break;
+            case 20:
+                balance += 20;
+                break;
+            case 50:
+                balance += 50;
+                break;
+            default:
+                System.out.println( coin + " is invalid currency unit!");
+        }
+    }
+
+    @Override
+    public int getBalance() {
+        return balance;
+    }
+
+    @Override
+    public Product purchaseProduct(int productId) {
+        if (productList) {
+            productList
+        }
+        return;
+    }
+
+    @Override
+    public int returnChange() {
+        int change = balance;
+        balance = 0;
+        return change;
+    }
+
+    @Override
+    public ArrayList<Product> getProducts() {
+        return productList;
+    }
 }
