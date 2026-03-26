0588baa057877e68b708ff417c39f7f81052dedb
Hans Malmefjäll <hans.malmefjall@gmail.com>
2026-03-26 11:17:32 +0100

    mod class files
    mod IVendingMachine.java add methods
    mod Product.java add fields and method

--- src/main/java/Lexicon/Hans/IVendingMachine.java
+++ src/main/java/Lexicon/Hans/IVendingMachine.java
@@ -1,4 +1,12 @@
 package Lexicon.Hans;
 
+import java.util.ArrayList;
+
 public interface IVendingMachine {
+
+    public void insertCoin(int coin);
+    public int getBalance();
+    public Product purchaseProduct(int productId);
+    public int returnChange();
+    public ArrayList<Product> getProducts();
 }
--- src/main/java/Lexicon/Hans/Product.java
+++ src/main/java/Lexicon/Hans/Product.java
@@ -1,4 +1,13 @@
 package Lexicon.Hans;
 
 public abstract class Product {
+
+    private int id;
+    private String name;
+    private int price;
+    private int quantity;
+
+    public String getDescription() {
+        return "Product id: " + id + ", name: " + name + ", price: " + price + ", quantity: " + quantity + ".";
+    }
 }
